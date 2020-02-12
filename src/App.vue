80225d4f617aff8876cd449f503c2b38210450d7
apc <alexcurran.va@gmail.com>
2020-02-12 10:18:51 -0600

    Navbar basics implemented

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,28 @@
 <template>
   <div id="app">
-    <nav class="nav">
-      <h2>Here is the navbar</h2>
-    </nav>
+    <header class="header--main">
+      <nav class="nav">
+        <router-link class="logo" :to="{ name: 'Home' }">Billow</router-link>
+        <ul class="nav__list">
+          <li class="nav__list__item">
+            <router-link class="nav__list__link" :to="{ name: 'Login' }">Login</router-link>
+          </li>
+          <li class="nav__list__item">
+            <router-link class="nav__list__link" :to="{ name: 'Signup' }">Signup</router-link>
+          </li>
+        </ul>
+      </nav>
+    </header>
     <router-view/>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
 h1,
@@ -30,6 +39,40 @@ input,
 textarea,
 a {
   font-family: 'Karla', sans-serif;
+}
+
+a {
+  text-decoration: none;
+  color: #222;
+}
+
+/* Navbar */
+.nav {
+  padding: 2.5rem 3rem;
+  background-color: #eee;
+  display: flex;
+  align-items: center;
+}
+
+.logo {
+  text-decoration: none;
+  color: #222;
+  font-size: 2rem;
+}
+
+.nav__list {
+  display: flex;
+  margin-left: 6rem;
+}
+
+.nav__list__item {
+  list-style: none;
+  margin: 0 .5rem;
+}
+
+.nav__list__link {
+  display: inline-block;
+  padding: .5rem;
 }
 
 </style>
