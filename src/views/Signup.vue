ee36cb8ecca838ab4233de9fd44229577b9fc059
apc <alexcurran.va@gmail.com>
2020-02-12 11:35:02 -0600

    Added signup submit to API

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,10 +1,70 @@
 <template>
-  <h1>Signup</h1>
+  <main>
+    <h1>Signup</h1>
+    <form @submit.prevent="signup" class="form-signup">
+      <p v-if="errorMessage" class="form__error">{{ errorMessage }}</p>
+      <div class="form-group">
+        <label for="name" class="form-group__label">Name</label>
+        <input v-model="user.name" type="text" class="form-group__input" name="name" required>
+      </div>
+      <div class="form-group">
+        <label for="email" class="form-group__label">Email</label>
+        <input v-model="user.email" type="email" class="form-group__input" name="email" required>
+      </div>
+      <div class="form-group">
+        <label for="password" class="form-group__label">Password</label>
+        <input v-model="user.password" type="password" class="form-group__input" name="password" required>
+      </div>
+      <input type="submit" class="form__submit" value="Signup">
+    </form>
+  </main>
 </template>
 
 <script>
 export default {
     name: "Signup",
+    data() {
+      return {
+        errorMessage: null,
+        user: {
+          name: "",
+          email: "",
+          password: ""
+        }
+      }
+    },
+    methods: {
+      async signup() {
+        
+        const API_SIGNUP_URL = "http://localhost:5000/user/signup";
+        const options = {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            name: this.user.name,
+            email: this.user.email,
+            password: this.user.password
+          })
+        };
+
+        try {
+          const response = await fetch(API_SIGNUP_URL, options);
+          const data = await response.json();
+          
+          this.checkForErrors(data);
+
+          if (this.errorMessage) return;
+
+          this.$router.push("/login");
+
+        } catch (err) {
+          console.error(err);
+        }
+      },
+      checkForErrors(resData) {
+        this.errorMessage = resData.message;
+      }
+    }
 }
 </script>
 
