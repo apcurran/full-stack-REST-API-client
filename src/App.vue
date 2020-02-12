9e2af86618d2dbb648fa4981a3679dc138b1bb81
apc <alexcurran.va@gmail.com>
2020-02-12 10:04:46 -0600

    Cleaned up pre-made component and view

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <nav class="nav">
+      <h2>Here is the navbar</h2>
+    </nav>
     <router-view/>
   </div>
 </template>
---
+++ src/views/Signup.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Signup</h1>
+</template>
+
+<script>
+export default {
+    name: "Signup",
+}
+</script>
+
+<style>
+
+</style>
