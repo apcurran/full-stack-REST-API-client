6f812b01a30e79df033a7af6a5c8295f84c40f22
apc <alexcurran.va@gmail.com>
2020-02-13 09:48:17 -0600

    Getting list of homes back on search page

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,15 +1,35 @@
 <template>
   <main>
       <h1>Search Page</h1>
+      <p v-for="home in homes" :key="home._id">{{ home }}</p>
   </main>
 </template>
 
 <script>
 export default {
     name: "Search",
+    data() {
+      return {
+        homes: []
+      }
+    },
+    async created() {
+      const API_SEARCH_URL = "http://localhost:5000/homes";
+      const options = {
+        headers: { "Content-Type": "application/json" }
+      };
+      const response = await fetch(API_SEARCH_URL, options);
+      const data = await response.json();
+
+      this.homes = data;
+    }
 }
 </script>
 
 <style>
+
+p {
+  margin: 1rem 0;
+}
 
 </style>
