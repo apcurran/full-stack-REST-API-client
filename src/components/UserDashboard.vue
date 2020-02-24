5d22e6aab589380dc1bb86c903946423308d74cb
apc <alexcurran.va@gmail.com>
2020-02-24 14:45:39 -0600

    Home deleting properly from db, now to delete from DOM before refresh

--- src/components/UserDashboard.vue
+++ src/components/UserDashboard.vue
@@ -2,28 +2,32 @@
   <section class="user-dash">
     <h3 class="user-dash__title">Your Favorite Homes</h3>
     <div class="homes-group">
-      <router-link :to="'/homes/' + home.home_id" class="home-group__card__info__link"  v-for="home in homes" :key="home.home_id">
-        <article class="homes-group__card">
-          <div class="homes-group__card__img-container">
-            <img :src="home.home_img_main" alt="House" class="homes-group__card__img">
-          </div>
-          <div class="home-group__card__info">
-            <h2 class="home-group__card__info__price">{{ formatPrice(home.home_price) }}</h2>
-            <h3 class="home-group__card__info__street">{{ home.home_street }},</h3>
-            <h3 class="home-group__card__info__state">{{ home.home_state }}</h3>
-            <ul class="home-group__card__info__desc-group">
-              <li>{{ home.home_bedrooms }} bds</li>
-              <li>{{ home.home_bathrooms }} ba</li>
-              <li>{{ home.home_squareFeet }} sqft</li>
-            </ul>
-          </div>
-        </article>
-      </router-link>
+        <div v-for="home in homes" :key="home.home_id" class="home-group__card__info__link-wrapper">
+            <router-link :to="'/homes/' + home.home_id" class="home-group__card__info__link">
+                <article class="homes-group__card">
+                <div class="homes-group__card__img-container">
+                    <img :src="home.home_img_main" alt="House" class="homes-group__card__img">
+                </div>
+                <div class="home-group__card__info">
+                    <h2 class="home-group__card__info__price">{{ formatPrice(home.home_price) }}</h2>
+                    <h3 class="home-group__card__info__street">{{ home.home_street }},</h3>
+                    <h3 class="home-group__card__info__state">{{ home.home_state }}</h3>
+                    <ul class="home-group__card__info__desc-group">
+                    <li>{{ home.home_bedrooms }} bds</li>
+                    <li>{{ home.home_bathrooms }} ba</li>
+                    <li>{{ home.home_squareFeet }} sqft</li>
+                    </ul>
+                </div>
+                </article>
+            </router-link>
+            <button @click="deleteHome(home.home_id)" class="home-delete-btn">Delete Home</button>
+        </div>
     </div>
   </section>
 </template>
 
 <script>
+
 export default {
     name: "UserDashboard",
     data() {
@@ -53,14 +57,29 @@ export default {
     methods: {
         formatPrice(homePrice) {
             const formatter = new Intl.NumberFormat("en-US", {
-            style: "currency",
-            currency: "USD"
+                style: "currency",
+                currency: "USD"
             });
 
             return formatter.format(homePrice);
+        },
+        async deleteHome(homeId) {
+            const API_FAVORITES_DELETE_URL = `http://localhost:5000/user/favorites/${homeId}`;
+            const options = {
+                method: "DELETE",
+                headers: {
+                    authorization: `Bearer ${localStorage.authToken}`,
+                    "Content-Type": "application/json"
+                }
+            };
+            const response = await fetch(API_FAVORITES_DELETE_URL, options);
+            const data = await response.json();
+
+            console.log(data);
         }
     }
 }
+
 </script>
 
 <style>
@@ -72,6 +91,17 @@ export default {
 .user-dash__title {
     margin: 1.5rem 0;
     font-size: 1.35rem;
+}
+
+.home-delete-btn {
+    margin: 1.5rem 0;
+    padding: .5rem 1.5rem;
+    font-size: 1rem;
+    color: #2d3748;
+    border: 1px solid #718096;
+    border-radius: 4px;
+    background-color: transparent;
+    cursor: pointer;
 }
 
 </style>
