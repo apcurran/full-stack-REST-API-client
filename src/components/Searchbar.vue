afe9f05c169e2622ea72311a958f177bd02990c1
apc <alexcurran.va@gmail.com>
2020-02-27 14:16:25 -0600

    Updated styling on searchbar

--- src/components/Searchbar.vue
+++ src/components/Searchbar.vue
@@ -1,6 +1,5 @@
 <template>
   <form @submit.prevent="searchHomes" class="searchbar-form">
-      <h1>Searchbar</h1>
       <p v-if="home.error" class="searchbar-form__error">{{ home.error }}</p>
       <div class="searchbar-form__field">
           <input v-model="home.search" type="text" class="searchbar-form__input" name="search" placeholder="Search by street, city, state, or zip code" required>
@@ -59,8 +58,51 @@ export default {
 <style>
 
 .searchbar-form {
-    margin: 2rem 0 5rem 0;
+    width: 40rem;
+    max-width: 92.5%;
+    margin: 2rem auto 5rem auto;
     text-align: center;
+}
+
+.searchbar-form__field {
+    display: flex;
+    justify-content: center;
+}
+
+.searchbar-form__input {
+    flex: 1 1 90%;
+    padding: .5rem 1.25rem;
+    border: none;
+    border-radius: 500px 0 0 500px;
+    outline: none;
+    font-size: 1rem;
+    background-color: #E2E8F0;
+
+    transition: background-color 125ms ease-in-out;
+}
+
+.searchbar-form__submit {
+    flex: 2 2 10%;
+    padding: .5rem;
+    border: none;
+    border-radius: 0 500px 500px 0;
+    outline: none;
+    cursor: pointer;
+    background-color: #E2E8F0;
+
+    transition: background-color 125ms ease-in-out;
+}
+
+.searchbar-form__input::placeholder {
+    color: #4A5568;
+}
+
+.searchbar-form__input:focus {
+    background-color: #EDF2F7;
+}
+
+.searchbar-form__input:focus + .searchbar-form__submit {
+    background-color: #EDF2F7;
 }
 
 </style>
