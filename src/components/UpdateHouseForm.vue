7eb8ba57f4f180f9d66bad6cab30e187a84e110f
apc <alexcurran.va@gmail.com>
2020-03-04 09:23:34 -0600

    Deleted JSON content-type from UpdateHouseForm component

--- src/components/UpdateHouseForm.vue
+++ src/components/UpdateHouseForm.vue
@@ -122,37 +122,39 @@ export default {
         }
       },
       async submitForm() {
-        const API_NEW_HOUSE_URL = "http://localhost:5000/homes/update";
+        const API_UPDATE_HOUSE_URL = "http://localhost:5000/homes/update";
+
+        let formData = new FormData();
+
+        formData.append("streetQuery", this.house.streetQuery);
+        formData.append("price", this.house.price);
+        formData.append("street", this.house.street);
+        formData.append("city", this.house.city);
+        formData.append("state", this.house.state);
+        formData.append("zip", this.house.zip);
+        formData.append("lat", this.house.lat);
+        formData.append("lon", this.house.lon);
+        formData.append("bedrooms", this.house.bedrooms);
+        formData.append("bathrooms", this.house.bathrooms);
+        formData.append("squareFeet", this.house.squareFeet);
+        formData.append("description", this.house.description);
+        formData.append("agent", this.house.agent);
+        formData.append("agent_img", this.house.agent_img);
+        formData.append("agent_phone", this.house.agent_phone);
+        formData.append("house_img_main", this.house.house_img_main);
+        formData.append("house_img_inside_1", this.house.house_img_inside_1);
+        formData.append("house_img_inside_2", this.house.house_img_inside_2);
+
         const options = {
           method: "PATCH",
           headers: {
             authorization: `Bearer ${localStorage.authToken}`,
-            "Content-Type": "application/json"
           },
-          body: JSON.stringify({
-            streetQuery: this.house.streetQuery,
-            price: this.house.price,
-            street: this.house.street,
-            city: this.house.city,
-            state: this.house.state,
-            zip: this.house.zip,
-            lat: this.house.lat,
-            lon: this.house.lon,
-            bedrooms: this.house.bedrooms,
-            bathrooms: this.house.bathrooms,
-            squareFeet: this.house.squareFeet,
-            description: this.house.description,
-            agent: this.house.agent,
-            agent_img: this.house.agent_img,
-            agent_phone: this.house.agent_phone,
-            house_img_main: this.house.house_img_main,
-            house_img_inside_1: this.house.house_img_inside_1,
-            house_img_inside_2: this.house.house_img_inside_2
-          })
+          body: formData
         };
 
         try {
-          const response = await fetch(API_NEW_HOUSE_URL, options);
+          const response = await fetch(API_UPDATE_HOUSE_URL, options);
           const data = await response.json();
 
           this.checkForErrors(data);
@@ -175,6 +177,7 @@ export default {
   font-size: 1.1rem;
   font-weight: 600;
   margin-right: .75rem;
+  color: var(--blue);
 }
 
 .form-house__input--update {
