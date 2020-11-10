f6b1bc8770b5fe5682c6738356606b009251ad4d
Alex Curran <alexcurran.va@gmail.com>
2020-11-10 14:23:48 -0600

    Inlined magnifying glass svg and adjusted stroke color to match bg color more, also updated npm packages

--- src/components/Searchbar.vue
+++ src/components/Searchbar.vue
@@ -3,7 +3,7 @@
       <div class="searchbar-form__field">
           <input v-model="home.searchTerm" type="text" class="searchbar-form__input" name="search" placeholder="Search by street, city, state, or zip code" required>
           <button type="submit" class="searchbar-form__submit">
-              <img src="../assets/icons/icon-search.svg" alt="Magnifying glass search icon" class="searchbar-form__submit__img" width="24" height="24">
+              <svg class="searchbar-form__submit__img" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
           </button>
       </div>
       <p v-if="home.error" class="searchbar-form__error">{{ home.error }}</p>
@@ -78,6 +78,12 @@ export default {
     outline: none;
     font-size: 1rem;
     background-color: #E2E8F0;
+}
+
+.searchbar-form__submit__img {
+    width: 1.5rem;
+    height: 1.5rem;
+    stroke: #718096;
 }
 
 .searchbar-form__submit {
